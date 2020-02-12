9a227404fe724d786ace3023d67d527985b34b89
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-02-12 10:51:44 +0000

    caculator.c commit

---
+++ caculator.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+float a,b,sum,sub,mul,div;
+printf("\n enter two numbers\n");
+scanf("%f%f",&a,&b);
+sum=a+b;
+printf("\n sum=%f",sum);
+sub=a-b;
+printf("\n subraction=%f",sub);
+mul=a*b;
+printf("\n multiplication=%f",mul);
+div=a/b;
+printf("\n division=%f",div);
+return (0);
+}
