7dcf97eab9f607def99d99f2e786122279f4fa34
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-02-14 06:42:11 +0000

    lab1.c write commit

--- lab1.c
+++ lab1.c
@@ -1,1 +1,14 @@
-//write your code here
+#include<stdio.h>
+int main()
+{
+	float si,p,t,r;
+	printf("\n enter principle:\n");
+	scanf("%f",&p);
+	printf("\n enter time:\n");
+	scanf("%f",&t);
+	printf("\n enter rate:\n");
+	scanf("%f",&r);
+	si=(p*t*r)/100;
+	printf("\n simple interest=%0.2f",si);
+	return (0);
+}
