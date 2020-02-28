59f32161c3bdc0eb4a6ce0193f6eddd3372dc7d2
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-02-28 06:52:55 +0000

    test2.c edited write commit

--- test2.c
+++ test2.c
@@ -1,1 +1,17 @@
-//write your code here
+#include<stdio.h>
+int total_convert_mins(int,int);
+int main()
+{
+	int hr,min,total_mins;
+	printf("enter hours and minutes:\n");
+	scanf("%d%d",&hr,&min);
+	total_mins=total_convert_mins(hr,min);
+	printf("total minutes=%d",total_mins);
+	return 0;
+}
+int total_convert_mins(int h,int m)
+{
+	int t;
+	t=h*60+m;
+	return t;
+}
