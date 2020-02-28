bef04a3753827712e17c3f0ad67d4906fb3f1d63
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-02-28 06:25:37 +0000

    lab2.c edited write commit

--- lab2.c
+++ lab2.c
@@ -1,1 +1,18 @@
-//write your code here
+#include<stdio.h>
+float area(int);
+int main()
+{
+	int r;
+	float ar;
+	printf("enter radius of he circle:\n");
+	scanf("%d",&r);
+	ar=area(r);
+	printf("area of circle:%f",ar);
+	return 0;
+}
+float area(int rad)
+{
+	float a;
+	a=3.14*rad*rad;
+	return a;
+}
