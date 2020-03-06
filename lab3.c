a0723edf29793d5017e5412ca06b9727a1c09c29
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-03-06 06:33:03 +0000

    lab3.c WRITE COMMIT

--- lab3.c
+++ lab3.c
@@ -1,1 +1,36 @@
-//write your code here
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("\n enter month  number\n");
+	scanf("%d",&n);
+	switch(n)
+	{
+		case 1: printf("january");
+			break;
+		case 2: printf("february");
+			break;
+		case 3: printf("march");
+			break;
+		case 4: printf("april");
+			break;
+		case 5: printf("may");
+			break;
+		case 6: printf("june");
+			break;
+		case 7: printf("july");
+			break;
+		case 8: printf("august");
+			break;
+		case 9: printf("september");
+			break;
+		case 10: printf("october");
+			break;
+		case 11: printf("november");
+			break;
+		case 12: printf("december");
+			break;
+		default: printf("\n wrong month number");
+	}
+	return 0;
+}
