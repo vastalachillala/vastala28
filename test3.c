ece5a7700742252c64747260c8d6314b416258e8
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-03-06 07:20:36 +0000

    test3.c write commit

--- test3.c
+++ test3.c
@@ -1,1 +1,17 @@
-//write your code here
+#include<stdio.h>
+int num(int,int,int);
+int main()
+{
+int a,b,c,large;
+printf("\n enter three numbers\n");
+scanf("%d%d%d",&a,&b,&c);
+large=num(a,b,c);
+printf("\n largest number=%d",large);
+return 0;
+}
+int num (int x,int y,int z)
+{
+int n;
+n=(x>y)?((x>z)?x:z):((y>z)?y:z);
+return n;
+}
