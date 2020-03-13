9ed6a0416e5ebb17638753aca01faed238ba6620
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-03-13 13:21:51 +0000

    assignment5.c write commit

--- assignment5.c
+++ assignment5.c
@@ -1,1 +1,32 @@
-//Enter your code here
+#include<stdio.h>
+int input();
+void compute();
+void output();
+int main()
+{
+	int a;
+	a=input();
+	output(a);
+	compute(a);
+	return 0;
+}
+int input()
+{
+	int n;
+	printf("\nenter n:\n");
+	scanf("%d",&n);
+	return n;
+}
+void output(int n)
+{
+	printf("\nfirst %d natural numbers:\n",n);
+}
+void compute(int n)
+{
+	int i=1;
+	while(i<=n)
+	{
+		printf("%d \n",i);
+		i++;
+	}
+}
