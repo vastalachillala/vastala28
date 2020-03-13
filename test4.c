beead979611b8a889a1f992ea637c324a7eac1b6
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-03-13 06:32:21 +0000

    test4.c write commit

--- test4.c
+++ test4.c
@@ -1,1 +1,15 @@
-//write your code here
+#include<stdio.h>
+int main()
+{
+	int n,sum=0,rem;
+	printf("\n enter number:\n");
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		rem=n%10;
+		sum=sum+rem;
+		n=n/10;
+	}
+	printf("\n sum of digits=%d",sum);
+	return 0;
+}
