697e20ec489a120631369ddca21e846218d6856b
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-03-13 07:14:54 +0000

    test4.c write commit

--- test4.c
+++ test4.c
@@ -1,15 +1,33 @@
 #include<stdio.h>
+void input();
+int compute(int);
+void output(int);
 int main()
 {
-	int n,sum=0,rem;
+	int in,out,com;
+	in=input();
+	com=compute(int in);
+	out=output(int com);
+	return 0;
+}
+void input()
+{
+	int n;
 	printf("\n enter number:\n");
 	scanf("%d",&n);
+}
+int compute(int n)
+{
+	int rem,sum=0;
 	while(n!=0)
 	{
 		rem=n%10;
 		sum=sum+rem;
 		n=n/10;
 	}
+	return sum;
+}
+void output(int sum)
+{	
 	printf("\n sum of digits=%d",sum);
-	return 0;
 }
