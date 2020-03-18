57945fcb713b52c93d677d31715cc1a58c20f733
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-03-18 09:55:20 +0000

    lab4.c write commit

--- lab4.c
+++ lab4.c
@@ -1,13 +1,27 @@
 #include<stdio.h>
+int input();
+int compute(int);
 int main()
 {
-	int i,n,a;
+	int in;
+	in=input();
+	compute(in);
+	return 0;
+}
+int input()
+{
+	int n;
 	printf("\n enter n:");
 	scanf("%d",&n);
+	return n;
+}
+int compute(int n)
+{
+	int a,i;
 	for(i=0;i<=100;i++)
 	{
 		a=i*n;
 		printf("\n %d \n",a);
 	}
-	return 0;
+	return a;
 }
