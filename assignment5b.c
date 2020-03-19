66fe89647fb4e7822ab4e2bc3f9596abaa044cfb
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-03-19 06:48:32 +0000

    assignment5b.c write commit

--- assignment5b.c
+++ assignment5b.c
@@ -1,15 +1,34 @@
 #include<stdio.h>
+int input();
+int compute(int);
+void output(float);
 int main()
 {
-    int i,n,sum=0;
-    float avg;
+    int in,com;
+    in=input();
+    com=compute(in);
+    output(com);
+    return 0;
+}
+int input()
+{
+    int n;
     printf("\n enter n:");
     scanf("%d",&n);
+    return n;
+}
+int compute(int n)
+{
+    int i,sum=0;
+    float avg;
     for(i=0;i<=n;i++)
     {
         sum=sum+i;
     }
     avg=sum/n;
+    return avg;
+}
+void output(float avg)
+{
     printf("\n average=%f",avg);
-    return 0;
 }
