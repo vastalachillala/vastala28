20f34fc403bfe415682ee8abd50754ce4e530488
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-03-21 20:13:52 +0000

    assignment5d.c write commit

---
+++ assignment5d.c
@@ -0,0 +1,101 @@
+1.
+    #include<stdio.h>
+    int main()
+{
+    int i,j;
+    for(i=1;i<=3;i++)
+    {
+        printf("\n");
+        for(j=1;j<=3;j++)
+        {
+            printf("@\t");
+        }
+    }
+    return 0;
+}
+
+
+2.
+    #include<stdio.h>
+    int main()
+{
+    int i,j;
+    for(i=1;i<=3;i++)
+    {
+        printf("\n");
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+    }
+    return 0;
+}
+
+
+3.
+    #include<stdio.h>
+    int main()
+{
+    int i,j;
+    for(i=1;i<=4;i++)
+    {
+        printf("\n");
+        for(j=1;j<=4;j++)
+        {
+            printf("%d\t",j);
+        }
+    }
+    return 0;
+}
+
+
+4.
+    #include<stdio.h>
+    int main()
+{
+    int i,j;
+    for(i=1;i<=4;i++)
+    {
+        printf("\n");
+        for(j=1;j<=i;j++)
+        {
+            printf("%d\t",j);
+        }
+    }
+    return 0;
+}
+
+
+5.
+    #include<stdio.h>
+    int main()
+{
+    int i,j;
+    {
+        for(i=1; i<=5; i++) 
+        {
+            for(j=1; j<=i; j++) 
+            {
+                printf("%d", i);
+            }
+            printf("\n");
+        }
+        return 0;
+}
+
+
+6.
+    #include <stdio.h>
+    int main() 
+{
+        int i, j;
+        for (i=1;i<=3;i++) 
+        {
+            printf("\n");
+            for (j=1;j<=3;j++) 
+            {
+                printf(" %c",'A'-1 + i);
+            }
+        }
+        return 0;
+}
