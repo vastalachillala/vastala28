124c9f04387dc0b500e75d8429b5e73d62d50521
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-03-27 18:16:55 +0000

    assignment6.c write commit

--- assignment6.c
+++ assignment6.c
@@ -1,1 +1,16 @@
-//Enter your code here
+#include<stdio.h>
+int main()
+{
+	int n,i,sum=0;
+	printf("\n enter number of elements");
+	scanf("%d",&n);
+	int ar[n];
+	printf("\n enter the elements");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&ar[i]);
+		sum=sum+ar[i];
+	}
+	printf("\n sum=%d",sum);
+	return 0;
+}
