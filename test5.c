7de353a12995a1aff746b1fc6cce99ed0981a591
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-03-27 18:09:03 +0000

    test5.c write commit

--- test5.c
+++ test5.c
@@ -1,1 +1,24 @@
-//write your code here
+#include<stdio.h>
+int main()
+{
+	int n,i,small,large;
+	printf("\n enter number of elements");
+	scanf("%d",&n);
+	int ar[n];
+	printf("\n enter elements");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&ar[i]);
+	}
+	small=large=ar[0];
+	for(i=0;i<n;i++)
+	{
+		if(ar[i]<small)
+		small=ar[i];
+		if(ar[i]>large)
+		large=ar[i];
+	}
+	printf("\n before interchange %d, %d",small,large);
+	printf("\n after interchange %d, %d",large,small);
+	return 0;
+}
