6766c8770180f44fdb5e2890f49d34a9792d0260
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-03-27 17:12:17 +0000

    lab5.c write commit

--- lab5.c
+++ lab5.c
@@ -1,1 +1,18 @@
-//write your code here
+#include<stdio.h>
+int main()
+{
+	int n,i,sum=0;
+	float avg;
+	printf("\n enter n");
+	scanf("%d",&n);
+	int ar[n];
+	printf("\n enter elements");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&ar[i]);
+		sum=sum+ar[i];
+	}
+	avg=sum/n;
+	printf("\n avegrage =%f",avg);
+	return 0;
+}
