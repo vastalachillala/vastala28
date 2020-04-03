a266c6bb5876c280c23bf14236b687746f026f01
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-04-03 09:40:25 +0000

    lab6.c write commit

--- lab6.c
+++ lab6.c
@@ -1,1 +1,34 @@
-//write your code here
+#include<stdio.h>
+int main()
+{
+	int a[10][10],m,n,i,j;
+	printf("\n enter rows and columns:");
+	scanf("%d%d",&m,&n);
+	printf("\n enter matrix elements:\n");
+	for(i=0;i<m;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("\n entered matrix:\n");
+	for(i=0;i<m;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			printf("%d ",a[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n transpose of the matrix:\n");
+	for(j=0;j<n;j++)
+	{
+		for(i=0;i<m;i++)
+		{
+			printf("%d\t",a[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
