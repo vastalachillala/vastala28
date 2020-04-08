7ff47dc21957981df6788fea149b86a43d2626e4
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-04-08 16:17:47 +0000

    test6.c write commit

--- test6.c
+++ test6.c
@@ -1,1 +1,29 @@
-//write your code here
+#include<stdio.h>
+int main()
+{
+	int m[2][5],i,j,k;
+	float sum=0;
+	for(i=0;i<5;i++)
+	{
+		printf("\n enter the sub %d marks of each sub:",i+1);
+		for(j=0;j<2;j++)
+		{
+			scanf("%d",&m[j][i]);
+		}
+	}
+	for(k=0;k<3;k++)
+	{
+		for(i=0;i<5;i++)
+		{
+			for(j=0;j<2;j++)
+			{
+				sum=sum+m[j][i];
+			}
+
+			printf("\n avg marks in sub %d is %f",i+1,sum/2);
+			sum=0;
+		}
+	}
+	printf("\n");
+	return 0;
+}
