03f59d6229c9625cd8aeb56e623d94fb9527edba
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-04-11 14:27:38 +0000

    test7.c write commit

--- test7.c
+++ test7.c
@@ -1,1 +1,27 @@
-//write your code here
+#include <stdio.h>
+int main()
+{
+	char str1[100],str2[100],str3[100];
+	int i,j;
+	printf("\n enter first string\n");
+	gets(str1);
+	printf("\n enter second string\n");
+	gets(str2);
+	i=0,j=0;
+	while(str1[i]!='\0')
+	{
+		str3[j]=str1[i];
+		i++;
+		j++;
+	}
+	i=0;
+	while(str2[i]!='\0')
+	{
+		str3[j]=str2[i];
+		i++;
+		j++;
+	}
+	str3[j]='\0';
+	printf("\n Concatenated string: %s",str3);
+	return 0;
+}
