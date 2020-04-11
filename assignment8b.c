3d75676b8875e16d09aac1bbddef38f8c3d94bf5
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-04-11 14:46:51 +0000

    assignment8b.c write commit

---
+++ assignment8b.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    int i;
+    printf("\n enter a string:");
+    gets(str);
+    i=0;
+    while(str[i++]!='\0')
+    {
+        if(str[i]>='a'&&str[i]<='z')
+            str[i]=str[i]-32;
+        else if(str[i]>='A'&&str[i]<='Z')
+            str[i]=str[i]+32;
+    }
+    printf("converted string:");
+    puts(str);
+    return 0;
+}
