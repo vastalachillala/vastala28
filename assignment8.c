f146031f07769b72db8fb8519161fcc0053d34d9
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-04-11 14:34:08 +0000

    assignment8.c write commit

--- assignment8.c
+++ assignment8.c
@@ -1,1 +1,14 @@
-//write your code here
+#include<stdio.h>
+int main()
+{
+	char str[100];
+	int len=0,i=0;
+	printf("\n enter a string:");
+	gets(str);
+	while(str[i++]!='\0')
+	{
+		len++;
+	}
+	printf("\n string length is : %d\n",len);
+	return 0;
+}
