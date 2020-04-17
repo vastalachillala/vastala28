0406f7c1c55ffbc37d0c593a5d7d20f06f766213
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-04-17 09:31:12 +0000

    test8.c write commit

--- test8.c
+++ test8.c
@@ -1,1 +1,27 @@
-//write your code here
+#include<stdio.h>
+int main()
+{
+    struct employee
+    {
+        char name[100];
+        char doj[100];
+        float sal;
+        int ID;
+
+    };
+    struct employee emp;
+    printf("Enter name of the employee \n");
+    scanf("%s",emp.name);
+    printf("Enter salary of the employee \n");
+    scanf("%f",&emp.sal);
+    printf("Enter the employee ID \n");
+    scanf("%d",&emp.ID);
+    printf("Enter the date of joining of the employee(dd/mm/yyyy) \n");
+    scanf("%s",emp.doj);
+    printf("*****EMPLOYEE DETAILS***** \n");
+    printf("Emp name: %s \n",emp.name);
+    printf("Emp Salary: %f \n",emp.sal);
+    printf("Emp ID: %d \n",emp.ID);
+    printf("Date of joining: %s \n",emp.doj);
+    return 0;
+}
