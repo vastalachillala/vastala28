5c3e318431a3e74362fd4a0ac6ff71b2e0b7e1eb
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-04-21 09:54:17 +0000

    lab8.c write commit

--- lab8.c
+++ lab8.c
@@ -2,35 +2,43 @@
 struct stud
 {
 	int s1,s2,rollno,tot;
-	char name[50],dept[50],sec[10];
+	char name[10],dept[10],sec[5];
 	double fees;
 }s[10];
 int main()
 {
-	int i;
+	int i,maxtot,pos;
 	for(i = 0; i < 2; i++)
 	{
-		printf("\n Enter the roll number of student %d : ",i+1);
+		printf("\n Enter the roll number of student :");
 		scanf("%d", &s[i].rollno);
 		printf("\n Enter the name : ");
 		scanf("%s", s[i].name);
 		printf("\n Enter section: ");
 		scanf("%s",s[i].sec);
 		printf("\n Enter department: ");
-		scanf("%s",s[i].dept); 
+		scanf("%s",s[i].dept);
 		printf("\n Enter fees: ");
 		scanf("%lf",&s[i].fees);
 		printf("\n Enter the total marks obtained: ");
 		scanf("%d", &s[i].tot);
 	}
-	printf("\nRoll No. \t Name \t Sec \t Dept \t Fees \t\t Total \n\n");
-	if(s[1].tot>s[2].tot)
+	maxtot=s[0].tot;
+	pos=0;
+	for(i=0;i<2;i++)
 	{
-		printf("%d \t %s \t %s \t %s \t %lf \t\t %d \n", s[1].rollno,s[1].name,s[1].sec,s[1].dept,s[1].fees,s[1].tot);
+		if(s[i].tot>maxtot)
+		{
+			maxtot=s[i].tot;
+			pos=i;
+		}
 	}
-	else
-	{
-		printf("%d \t %s \t %s \t %s \t %lf \t\t %d \n", s[2].rollno,s[2].name,s[2].sec,s[2].dept,s[2].fees,s[2].tot);
-	}
+	printf("details of the students with the higher marks are\n");
+	printf("roll no: %d\n", s[pos].rollno);
+	printf("name: %s\n", s[pos].name);
+	printf("section: %s\n",s[pos].sec);
+	printf("department: %s\n",s[pos].dept);
+	printf("fees: %lf\n",s[pos].fees);
+	printf("total marks: %d\n",s[pos].tot);
 	return 0;
 }
