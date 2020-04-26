9b71932349c237a931e4d2257c874528e336275d
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-04-26 13:14:19 +0000

    test3.c write commit

--- test3.c
+++ test3.c
@@ -1,17 +1,36 @@
 #include<stdio.h>
-int num(int,int,int);
+void input(int *a,int *b,int *c)
+{
+    printf("\n enter 3 integer: ");
+    scanf("%d%d%d",&*a,&*b,&*c);
+}
+int compute(int a,int b,int c)
+{
+    int s;
+    if((a>=b)&&(a>=c))
+    {
+        s=a;
+    }
+    else if(b>=c)
+    {
+        s=b;
+    }
+    else
+    {
+        s=c;
+    }
+    return s;
+}
+void output(int a,int b,int c,int s)
+{
+    printf("\n the largest of %d, %d and %d is %d",a,b,c,s);
+    printf("\n");
+}
 int main()
 {
-	int a,b,c,large;
-	printf("\n enter three numbers\n");
-	scanf("%d%d%d",&a,&b,&c);
-	large=num(a,b,c);
-	printf("\n largest number=%d",large);
-	return 0;
-}
-int num (int x,int y,int z)
-{
-	int n;
-	n=(x>y)?((x>z)?x:z):((y>z)?y:z);
-	return n;
+    int a,b,c,s;
+    input(&a,&b,&c);
+    s=compute(a,b,c);
+    output(a,b,c,s);
+    return 0;
 }
