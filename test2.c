d491c64992ee78aa05aaa26c103ba53c065fb389
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-04-26 13:11:27 +0000

    test2.c write commit

--- test2.c
+++ test2.c
@@ -1,17 +1,36 @@
 #include<stdio.h>
-int total_convert_mins(int,int);
+struct time
+{
+    int hr;
+    int min;
+};
+typedef struct time data;
+data input()
+{
+    data a;
+    printf("\n enter hours: ");
+    scanf("%d",&a.hr);
+    printf("\n enter minutes: ");
+    scanf("%d",&a.min);
+    return a;
+}
+int total_convert_mins(data a)
+{
+    int t;
+    t=a.hr*60+a.min;
+    return t;
+}
+void output(data a,int t)
+{
+    printf("\n total minutes in %d hours and %d minutes is %d minutes",a.hr,a.min,t);
+    printf("\n");
+}
 int main()
 {
-	int hr,min,total_mins;
-	printf("enter hours and minutes:\n");
-	scanf("%d%d",&hr,&min);
-	total_mins=total_convert_mins(hr,min);
-	printf("total minutes=%d",total_mins);
-	return 0;
-}
-int total_convert_mins(int h,int m)
-{
-	int t;
-	t=h*60+m;
-	return t;
+    data a;
+    int com;
+    a=input();
+    com=total_convert_mins(a);
+    output(a,com);
+    return 0;
 }
