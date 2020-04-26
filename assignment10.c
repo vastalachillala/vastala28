deeeddc3ed3b54e29fe8d66fc69f955891789d8d
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-04-26 16:17:52 +0000

    assignment10.c write commit

--- assignment10.c
+++ assignment10.c
@@ -1,1 +1,20 @@
-//Enter your code here
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	char *str;
+	int i = 0;
+	printf("\n enter a string: ");
+	str=calloc(sizeof(char),50);
+	fgets(str,50,stdin);
+	while(*(str+i)!='\0')
+	{
+		if(*(str+i)>=97 && *(str+i)<=122)
+		{
+			*(str+i)=*(str+i)-32;
+		}
+		i++;
+	}
+	printf("%s",str);
+	return 0;
+}
