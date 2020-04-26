41eb78b28dedd86e6e6ed7a4dcc261a7468acd81
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-04-26 06:16:08 +0000

    lab1.c write commit

--- lab1.c
+++ lab1.c
@@ -1,14 +1,38 @@
 #include<stdio.h>
+struct interestscheme
+{
+	int p;
+	float t;
+	float r;
+};
+typedef struct interestscheme Data;
+Data input()
+{
+	Data a;
+	printf("\n enter principal value: ");
+	scanf("%d",&a.p);
+	printf("\n enter the time period in years: ");
+	scanf("%f",&a.t);
+	printf("\n enter rate: ");
+	scanf("%f",&a.r);
+	return a;
+}
+float compute(Data a)
+{
+	float s;
+	s=(a.p*a.t*a.r)/100;
+	return s;
+}
+void output(Data a,float s)
+{
+	printf("\n For the given\n Principle value:%d\n Time:%0.2f\n Rate:%0.2f\n simple interest is %0.2f",a.p,a.t,a.r,s);
+}
 int main()
 {
-	float si,p,t,r;
-	printf("\n enter principle:\n");
-	scanf("%f",&p);
-	printf("\n enter time:\n");
-	scanf("%f",&t);
-	printf("\n enter rate:\n");
-	scanf("%f",&r);
-	si=(p*t*r)/100;
-	printf("\n simple interest=%0.2f",si);
-	return (0);
+	Data a;
+	float s;
+	a=input();
+	s=compute(a);
+	output(a,s);
+	return 0;
 }
