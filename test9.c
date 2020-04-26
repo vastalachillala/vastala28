fce4be76c98e8861b48ab7197ad56ba5edc9bd0f
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-04-26 15:55:54 +0000

    test9.c write commit

--- test9.c
+++ test9.c
@@ -1,1 +1,32 @@
-//write your code here
+#include<stdio.h>
+void swap(int, int);
+void swap1(int *,int *);
+int main()
+{
+	int a,b;
+	printf("\n Enter two numbers: ");
+	scanf("%d%d",&a,&b);
+	swap(a,b);
+	printf("\n using call by value:");
+	printf("\n Value of a= %d",a);
+	printf("\n Value of b= %d",b);
+	swap1(&a,&b);
+	printf("\n using call by reference:");
+	printf("\n Value of a= %d",a);
+	printf("\n Value of b= %d",b);
+	return 0;
+}
+void swap(int p, int q)
+{
+	int temp;
+	temp=p;
+	p=q;
+	q=temp;
+}
+void swap1(int *p, int *q)
+{
+	int temp;
+	temp=*p;
+	*p=*q;
+	*q=temp;
+}
