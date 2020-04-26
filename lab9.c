1f580f9b9c19059d8253ffd781a57e58b3e43206
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-04-26 14:39:56 +0000

    lab9.c write commit

--- lab9.c
+++ lab9.c
@@ -1,1 +1,19 @@
-//write your code here
+#include<stdio.h>
+int main()
+{
+	int a,b,*p,*q;
+	p=&a;
+	q=&b;
+	printf("\n Enter two numbers: ");
+	scanf("%d%d",&a,&b);
+	printf("\n Value pointed by p= %d",*p);
+	printf("\n Value pointed by q= %d",*q);
+	printf("\n Address of a= %d",q);
+	printf("\n Address of b= %d",p);
+	printf("\n Addition= %d",(*p)+(*q));
+	printf("\n Difference= %d",(*p)-(*q));
+	printf("\n Product= %d",(*p)*(*q));
+	printf("\n Quotient= %d",(*p)/(*q));
+	printf("\n Remainder= %d",(*p)%(*q));
+	return 0;
+}
