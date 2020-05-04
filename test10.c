0eb0cda55cefe9eb7f8f7a60e99ce0aada92f4d0
Vastala Chillala Narayana <vastala.se19@bmsce.ac.in>
2020-05-04 09:09:34 +0000

    test10.c write commit

--- test10.c
+++ test10.c
@@ -1,1 +1,35 @@
-//write your code here
+#include<stdio.h>
+#include<file.h>
+int main()
+{
+	FILE *f1,*f2;
+	char c;
+	printf(“data input\n\n”)
+		f1 = fopen(“INPUT”, “w”);
+	while((c = getchar())!=EOF)
+		putc(c,f1);
+	fclose(f1);
+	printf(“\ndata output\n\n”)
+		f1 = fopen(“INPUT”, “r”);
+	while((c = getchar())!=EOF)
+		putc(“%c”,c);
+	fclose(f1);
+	f1=fopen(file1,”r”)
+		if(f1=NULL)
+		{
+			printf(“no data”);
+			exit(0):
+		}
+	f2=fopen(file2,”w”);
+	if(f2=null)
+	{
+		printf(“cannot able to open”);
+		exit(0);
+	}
+	while((ch==getc(f1)!=EOF)
+		  putc(ch,f2);
+		  printf(“completed”);
+		  fclose(f1);
+		  fclose(f2);
+		  return 0;
+		  }
